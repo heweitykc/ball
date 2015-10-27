9aafd73c26180e50079fff2b53db57a02d5a71b6
callee <heweitykc@163.com>
2015-10-28 00:26:14 +0800

    通讯代码

--- proj/smashprj/Assets/network/server/main.go
+++ proj/smashprj/Assets/network/server/main.go
@@ -5,11 +5,76 @@ import (
     "fmt"
     "log"
     "net/http"
+	"time"
+	"strconv"
 )
- 
+
+var GlobalRooms = make(map[string]*Room)
+var roomIndex int = 0
+
+type Room struct{
+	id int
+	player0 *websocket.Conn
+	player1 *websocket.Conn
+}
+
+func (m *Room) Init(){
+	m.player0 = nil
+	m.player1 = nil
+	go m.Update()
+}
+
+func (m *Room) AddPlayer(player *websocket.Conn){
+	if m.player0 == nil {
+		m.player0 = player
+		return
+	}
+	if m.player1 == nil {
+		m.player1 = player
+		return
+	}
+}
+
+func (m *Room) IsFull() bool{
+	if m.player0 != nil && m.player1 != nil {
+		return true
+	}
+	return false
+}
+
+func (m *Room) Exec(cmd string, player *websocket.Conn){
+	
+}
+
+func (m *Room) Update(){
+	for {
+		fmt.Println(m.id," run...")
+		time.Sleep(3*time.Second)	
+	}
+}
+
+//查找房间
+func SearchRoom(player *websocket.Conn) *Room{
+	for _,room := range GlobalRooms{
+		if room.IsFull() == false {
+			room.AddPlayer(player)
+			return room;
+		}
+	}
+	room := new(Room)
+	room.id = roomIndex
+	room.Init()
+	room.AddPlayer(player)
+	GlobalRooms[string(roomIndex)] = room
+	roomIndex++
+	fmt.Println("add new room ", room.id)
+	return room
+}
+
+
 func Echo(ws *websocket.Conn) {
     var err error
- 
+	room := SearchRoom(ws)
     for {
  
         var reply string
@@ -21,13 +86,13 @@ func Echo(ws *websocket.Conn) {
  
         fmt.Println("Received back from client: " + reply)
  
-        msg := "Received:  " + reply
-        fmt.Println("Sending to client: " + msg)
+        msg := "Received:  " + strconv.Itoa(room.id) + " " + reply 
+        fmt.Println("Sending to client: " + msg + " ")
  
         if err = websocket.Message.Send(ws, msg); err != nil {
             fmt.Println("Can't send")
             break
-        }
+        }		
     }
 }
  
